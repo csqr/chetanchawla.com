0e434a95195e4c7b69ce148906f088a0516f72a8
Chetan <chetanchawla@gmail.com>
2021-07-25 15:38:52 -0500

    Playing around with Blogdown, pre-customized site - July 25, 2021.

---
+++ .Rprofile
@@ -0,0 +1,33 @@
+# REMEMBER to restart R after you modify and save this file!
+
+
+
+# First, execute the global .Rprofile if it exists. You may configure blogdown
+# options there, too, so they apply to any blogdown projects. Feel free to
+# ignore this part if it sounds too complicated to you.
+if (file.exists("~/.Rprofile")) {
+  base::sys.source("~/.Rprofile", envir = environment())
+}
+
+
+
+#Removed from Options:
+# build .Rmd to .html (via Pandoc); to build to Markdown, set this option to 'markdown'
+#blogdown.method = 'html'
+# to disable knitting Rmd files on save, set this option to FALSE
+#blogdown.knit.on_save = TRUE,
+# to automatically serve the site on RStudio startup, set this option to TRUE
+#blogdown.serve_site.startup = FALSE,
+
+# fix Hugo version
+options(blogdown.hugo.version = "0.85.0")
+
+#General default settings
+# https://developer.r-project.org/Blog/public/2020/02/16/stringsasfactors/ 
+
+options(stringsAsFactors = FALSE) 
+
+options(scipen=999) #preventing exponents in output
+
+options(dplyr.width = Inf) #to show all variable columns in head()
+
---
+++ Website coding.R
@@ -0,0 +1,38 @@
+#Website using blogdown: July 24, 2021, https://alison.rbind.io/blog/2020-12-new-year-new-blogdown/ 
+library(blogdown)
+# Step 3: Create Site
+#Comments in https://alison.rbind.io/blog/2020-12-new-year-new-blogdown/ , template renamed "starter-hugo-academic"
+
+new_site(theme = "wowchemy/ starter-hugo-academic ")
+blogdown::new_post(title = "Hello Hugo", 
+                   ext = '.Rmarkdown', 
+                   subdir = "post")
+#A rule that is true 90% of the time: folders in content/ are singular, not plural— ✔️ post; ❌ posts 
+#You can add an option to your .Rprofile to save these settings so you don’t have to remember them:
+# if exists, opens; if not, creates new
+blogdown::config_Rprofile() 
+
+#After restart, serving site again.
+
+serve_site()
+
+#Getting error: Error in options(blogdown.author = "Chetan Chawla", 
+# blogdown.ext = ".Rmarkdown",  : argument is missing, with no default 
+# https://github.com/rbind/apreshill/issues/44 
+# Removed .Rprofile customization, then installed latest version of blogdown
+
+library(utils)
+remotes::install_github('rstudio/blogdown')
+
+update.packages(ask = FALSE, checkBuilt = TRUE)
+
+blogdown::hugo_version()
+# Using Github: https://alison.rbind.io/blog/2020-12-new-year-new-blogdown/#using-github 
+#make a .gitignore file:
+file.edit(".gitignore")
+
+#Checks
+
+blogdown::check_gitignore()
+
+blogdown::check_content()
